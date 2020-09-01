c7ec39897772b7b566a9673ffd37b1d90ae672ae
Ilja Lapkovsky <Ilja.Lapkovsky@ecentria.com>
2020-09-01 14:54:36 +0300

    Upgrade to Symfony 5.1

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Ecentria group, inc. software.
- *
- * (c) 2020, Ecentria group, inc
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                      => ['all' => true],
@@ -20,4 +11,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class                => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class                    => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Ecentria\Bundle\CrontabBundle\EcentriaCrontabBundle::class       => ['all' => true],
 ];
